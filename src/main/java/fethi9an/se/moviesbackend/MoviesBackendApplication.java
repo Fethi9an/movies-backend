21716791197c3b0ac5c6cc2172a1b3fe90a3d645
fethi9an <martinfethi97@hotmail.com>
2023-03-08 16:40:54 +0100

    controller added

--- src/main/java/fethi9an/se/moviesbackend/MoviesBackendApplication.java
+++ src/main/java/fethi9an/se/moviesbackend/MoviesBackendApplication.java
@@ -6,15 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MoviesBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesBackendApplication.class, args);
 	}
-	@GetMapping("/")
-	public String apiIndex(){
 
-		return "hello world";
-	}
 }
